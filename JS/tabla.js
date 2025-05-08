aeaa31bbaa9dd7d22df1f366fd77913626a18ad4
Miguel Herrera <herreralag34@hotmail.com>
2025-05-07 22:28:32 -0700

    Se creo la pagina 'Lista Alumnos'

---
+++ JS/tabla.js
@@ -0,0 +1,11 @@
+//Obtención de obj
+const txtId = document.getElementById('id');
+const txtNombre = document.getElementById('nombre');
+const txtEscuela = document.getElementById('escuela');
+const btnAgregar = document.getElementById('btnAgregar');
+const tabla = document.getElementById('tabla')
+const tavlaBody = document.getElementById('tablaBody')
+
+function newColumnas() {
+
+}
