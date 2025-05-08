9c5cea552af1cba8d1d2f3d6008dd6dc3e4b6a95
Miguel Herrera <herreralag34@hotmail.com>
2025-05-08 15:59:18 -0700

    Se finalizo el .js de Cotización

--- JS/cotizacion.js
+++ JS/cotizacion.js
@@ -2,7 +2,11 @@
 const numCotizacion = document.getElementById('numCotizacion');
 const porPagoInicial = document.getElementById('porPagoInicial');
 const precio = document.getElementById('Precio');
-const plazos = document.getElementById('idPlazos');
+const plazo12 = document.getElementById('plazos12');
+const plazo24 = document.getElementById('plazos24');
+const plazo36 = document.getElementById('plazos36');
+const plazo48 = document.getElementById('plazos48');
+const plazo60 = document.getElementById('plazos60');
 
 
 /**Resultados */
@@ -20,12 +24,35 @@ const obtenerPlazo = () =>{
 };
 
 
-/**Metodos de Comportamiento */
-const calcularPagoInicial = (pagoInicial = precio * (porPagoInicial/100)) => pagoInicial;
+/**Funciones de Comportamiento */
+const calcularPagoInicial = (pagoInicial = precio.value * (porPagoInicial.value/100)) => pagoInicial;
 
-const calcularTotalFin = (totalFin = precio - calcularPagoInicial()) => totalFin;
+const calcularTotalFin = (totalFin = precio.value - calcularPagoInicial()) => totalFin;
 
-const calcularPagoMensual = (pagoMensual = calcularTotalFin()/plazos) => pagoMensual;
+const calcularPagoMensual = (pagoMensual) =>{
+    let plazo;
+    switch(true){
+        case plazo12.checked:
+            plazo = 12;
+            break;
+        case plazo24.checked:
+            plazo = 24;
+            break;
+        case plazo36.checked:
+            plazo = 36;
+            break;
+        case plazo48.checked:
+            plazo = 48;
+            break;
+        case plazo60.checked:
+            plazo = 60;
+            break;
+        default:
+            plazo = 0;
+    }
+    pagoMensual = calcularTotalFin() / plazo;
+    return pagoMensual;
+};
 
 
 /** Funciones finales*/
@@ -33,6 +60,10 @@ const realizarCalculos = () => {
     const pagoInicial = calcularPagoInicial();
     const totalFinanciar = calcularTotalFin();
     const pagoMensual = calcularPagoMensual();
+
+    alert(pagoInicial);
+    alert(totalFinanciar);
+    alert(pagoMensual);
 
     resPagoInicial.value = pagoInicial.toFixed(2);
     resTotalFinanciar.value = totalFinanciar.toFixed(2);
@@ -46,9 +77,11 @@ const limpiarCampos = () => {
     porPagoInicial.value = "";
     precio.value = "";
     
-    for(const radioPlazo of plazos){
-        plazos.checked = false;
-    }
+    plazo12.checked = false;
+    plazo24.checked = false;
+    plazo36.checked = false;
+    plazo48.checked = false;
+    plazo60.checked = false;
 
     resPagoInicial.value = "";
     resTotalFinanciar.value = "";
