dbd2bc7163210dd7c31ce4f876ab24bdc26ede6f
Miguel Herrera <herreralag34@hotmail.com>
2025-05-12 09:25:14 -0700

    Se finalizo la pagina 'Lista Alumnos'

--- JS/tabla.js
+++ JS/tabla.js
@@ -4,8 +4,39 @@ const txtNombre = document.getElementById('nombre');
 const txtEscuela = document.getElementById('escuela');
 const btnAgregar = document.getElementById('btnAgregar');
 const tabla = document.getElementById('tabla')
-const tavlaBody = document.getElementById('tablaBody')
+const tablaBody = document.getElementById('tablaBody')
 
-function newColumnas() {
+function agregarAlumno(){
+    const id = txtId.value;
+    const nombre = txtNombre.value;
+    const escuela = txtEscuela.value;
 
+    if(!id || !nombre || !escuela){
+        alert('Por favor, complete todos los campos');
+        return;
+    }
+
+    const nuevaFila = document.createElement('tr'); /**Nueva Fila */
+
+    const celdaId = document.createElement('td');/*Celdas para ID, Nombre y Escuela*/
+    celdaId.textContent = id;
+    const celdaNombre = document.createElement('td');
+    celdaNombre.textContent = nombre;
+    const celdaEscuela = document.createElement('td');
+    celdaEscuela.textContent = escuela;
+
+    /**Añadir celdas a la fila */
+    nuevaFila.appendChild(celdaId);
+    nuevaFila.appendChild(celdaNombre);
+    nuevaFila.appendChild(celdaEscuela);
+    
+    /**Añadir fila al cuerpo d la tabla */
+    tablaBody.appendChild(nuevaFila);
+
+    txtId.value = '';   /**Limpiar despues de agregar campos */
+    txtNombre.value = '';
+    txtEscuela.value = '';
+    txtId.focus();
 }
+
+btnAgregar.addEventListener('click', agregarAlumno); /**Evento click */
